e3670507715f57992c484b8239bd9e87eca01e1e
sushant692 <48211388+sushant692@users.noreply.github.com>
2019-08-09 18:16:33 +0530

    Add files via upload

---
+++ src/app/Search-Data.ts
@@ -0,0 +1,8 @@
+import { Search } from './search';
+export const SEARCH : Search[] = [
+	{SearchElement:'Broccoli', val:[50,42,40,33],SearchTime:''},
+	{SearchElement:'Sweet Potato',val:[30,35,40,42],SearchTime:''},
+	{SearchElement:'Capscicum',val:[35,32,34,30],SearchTime:''},
+	{SearchElement:'Radish',val:[18,16,20,35],SearchTime:''},
+	{SearchElement:'Potato',val:[10,13,12,10],SearchTime:''}
+];
